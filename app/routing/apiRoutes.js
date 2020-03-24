f531e1b48e54e95d28ae2537df85a4e59be1d5ea
iamjink <jinijinjin@gmail.com>
2020-03-24 03:13:14 -0700

    putting difference calculation in apirouts.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,42 @@
 var friends = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/friendslist", function(req,res) {
+module.exports = function (app) {
+    app.get("/api/friendslist", function (req, res) {
         res.json(friends);
     });
+
+    //new API
+    app.post("/api/friendslist", function (req, res) {
+        var userInput = req.body;
+        var userScores = userInput.score;
+
+        var matchName = "";
+        var matchPhoto = "";
+
+        var totalDifference = 10000;
+
+        for (var i = 0; i < friends.length; i++) {
+            var diff = 0;
+            for (var j = 0; j < userScores.length; j++) {
+                diff += Math.abs(friends[i].score[j] - userScores[j]);
+            }
+            if (diff < totalDifference) {
+
+                totalDifference = diff;
+                matchName = friends[i].userName;
+                matchPhoto = friends[i].userPhoto;
+                console.log(totalDifference);
+                // console.log(matchName);
+                // console.log(matchPhoto);
+
+            }
+        }
+        friends.push(userInput);
+        res.json({
+            status: 'OK',
+            matchName: matchName,
+            matchPhoto: matchPhoto
+        });
+        // console.log(userInput)
+    })
 }
